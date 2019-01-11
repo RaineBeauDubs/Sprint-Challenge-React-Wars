d0c3a65c76cbb93f6c85fc37e83013f4d52b7c65
Raine Wallace <rainebeaudubs@gmail.com>
2019-01-11 17:45:49 -0500

    Got the *if I run out of time, at least this looks decent* css done, now to work on the cool stuff.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import './StarWars.css';
 
-function Character (props) {
+function Character(props) {
   return (
-  <div className="person" key={props.char.toString()}>
-        <div className="main-stats">
-          {props.char.name}
-          {props.char.gender}
-          {props.char.birth_year}
-        </div>
-        <div className="minor-stats">
-          {props.char.hair_color}
-          {props.char.eye_color}
-          {props.char.height}
-          {props.char.mass}
-        </div>
-      </div> 
+    <div className="person" key={props.char.toString()}>
+      <div className="stats">
+        
+          <p className="name">Name: {props.char.name}</p>
+          <p>Gender: {props.char.gender}</p>
+          <p>Year of Birth: {props.char.birth_year}</p>
+        
+       
+          <p>Hair Color: {props.char.hair_color}</p>
+          <p>Eye Color: {props.char.eye_color}</p>
+          {/* <p>{props.char.height}</p> */}
+          {/* <p>{props.char.mass}</p> */}
+      </div>
+    </div>
   );
 }
 
